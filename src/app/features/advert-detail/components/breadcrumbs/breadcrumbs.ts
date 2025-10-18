7c81eff0ffde75e783b03be638a5255cd845d6e4
qq qqq <dansd5@mail.ru>
2025-10-19 01:29:41 +0300

    feat(details): added details page

---
+++ src/app/features/advert-detail/components/breadcrumbs/breadcrumbs.ts
@@ -0,0 +1,42 @@
+import { Component, inject, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { BreadcrumbsService } from './services/breadcrumbs-service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Observable, of, switchMap } from 'rxjs';
+import { Category } from '@/shared/types/category';
+
+interface BreadcrumbItem {
+    id: string;
+    name: string;
+}
+@Component({
+    selector: 'app-breadcrumb',
+    standalone: true,
+    imports: [CommonModule, RouterModule],
+    templateUrl: './breadcrumbs.html',
+    styleUrls: ['./breadcrumbs.scss']
+})
+export class Breadcrumbs {
+    categoryId = input.required<string>();
+    private categoriesBusiness = inject(BreadcrumbsService);
+    breadcrumbs = toSignal<BreadcrumbItem[]>(
+        this.categoriesBusiness.getAll().pipe(
+            switchMap(categories => this.buildBreadcrumb(categories, this.categoryId()))
+        )
+    );
+    private buildBreadcrumb(categories: Category[], id: string): Observable<BreadcrumbItem[]> {
+        const chain: BreadcrumbItem[] = [];
+        let currentId = id;
+        while (currentId && currentId !== '00000000-0000-0000-0000-000000000000') {
+            const cat = categories.find(c => c.id === currentId);
+            if (cat) {
+                chain.unshift({ id: cat.id, name: cat.name });
+                currentId = cat.parentId;
+            } else {
+                break;
+            }
+        }
+        return of(chain);
+    }
+}
---
+++ src/app/features/advert-detail/components/breadcrumbs/services/breadcrumbs-api.ts
@@ -0,0 +1,14 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Category } from '@/shared/types/category';
+import { environment } from '@/environments/environment';
+@Injectable({
+    providedIn: 'root'
+})
+export class BreadcrumbsApi {
+    private http = inject(HttpClient);
+    getAll(): Observable<Category[]> {
+        return this.http.get<Category[]>(`${environment.baseUrl}/Categories`);
+    }
+}
